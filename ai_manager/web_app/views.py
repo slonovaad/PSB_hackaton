e64a0b146984860a2e0b2bde0fcb202918ba11c6
Слонова Анна <mai.slonovaad@gmail.com>
2025-11-28 20:26:31 +0300

    Connect with base form front

--- ai_manager/web_app/views.py
+++ ai_manager/web_app/views.py
@@ -3,12 +3,15 @@ from llm.llm_main import llm_bank_employee
 from web_app.forms import LetterForm
 import json
 
+
 # Create your views here.
 def index(request):
     query = ""
     f = LetterForm()
+    author = ""
+    letter = ""
     if request.method == "GET":
-        #query = request.GET.get('ask', 'Кто ты?')
+        # query = request.GET.get('ask', 'Кто ты?')
         return render(request, "index.html", {"form": f, "is_post": False})
     if request.method == "POST":
         f = LetterForm(request.POST)
@@ -24,5 +27,7 @@ def index(request):
                "answer": res["answer"],
                "type": res["type"],
                "form": f,
+               "author": author,
+               "letter": letter,
                "is_post": True}
     return render(request, 'index.html', context)
