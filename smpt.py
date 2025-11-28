d0e2c277e37b447be61363fc95f38a8cf06bc3de
Artem <art240871@gmail.com>
2025-11-29 00:31:09 +0300

    add smpt.py for send message

---
+++ smpt.py
@@ -0,0 +1,54 @@
+# smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+# smtpObj.starttls()
+# smtpObj.login('hacatontest@gmail.com','izgu gpfq ipzq orkt')
+# # smtpObj.sendmail("hacatontest@gmail.com", "hacatontest@gmail.com", "text")
+# help(smtpObj)
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+
+def send_mail(mail: str, subject: str, text: str, *args: str):
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+    username = "hacatontest@gmail.com"
+    password = "izgu gpfq ipzq orkt"
+
+    msg = MIMEMultipart()
+    msg['From'] = username
+    msg['To'] = mail 
+    msg['Subject'] = subject
+
+    body = text
+    msg.attach(MIMEText(body, 'plain'))
+    try:
+        for item in args:
+            filename = item
+            attachment = open(filename, "rb")
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(attachment.read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition', f"attachment; filename= {filename}")
+            msg.attach(part)
+    except:
+        
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        msg.attach(part)
+
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(username, password)
+        text = msg.as_string()
+        server.sendmail(username, mail, text)
+        server.quit()
+        print("Письмо отправлено!")
+    except Exception as e:
+        print(f"Ошибка: {e}")
+
+send_mail("hacatontest@gmail.com", "test", "text in body", "file.txt", "file2.txt")
